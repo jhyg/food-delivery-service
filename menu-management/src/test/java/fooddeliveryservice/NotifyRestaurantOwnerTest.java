9398a3ad559706b54ebd54e1db3f179a9ebb3a8c
jhyg <65217813+jhyg@users.noreply.github.com>
2023-12-22 16:20:36 +0900

    commit

---
+++ menu-management/src/test/java/fooddeliveryservice/NotifyRestaurantOwnerTest.java
@@ -0,0 +1,108 @@
+package fooddeliveryservice;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fooddeliveryservice.config.kafka.KafkaProcessor;
+import fooddeliveryservice.domain.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.context.ApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.MimeTypeUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class NotifyRestaurantOwnerTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+        EventTest.class
+    );
+
+    @Autowired
+    private KafkaProcessor processor;
+
+    @Autowired
+    private MessageCollector messageCollector;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    public MenuRegisteredRepository repository;
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void test0() {
+        //given:
+        MenuRegistered entity = new MenuRegistered();
+
+        entity.setMenuId("1");
+        entity.setName("메뉴1");
+        entity.setPrice(new Money(10.0, "KRW"));
+        entity.setDescription("메뉴1의 설명");
+
+        repository.save(entity);
+
+        //when:
+
+        OrderPlaced event = new OrderPlaced();
+
+        event.setOrderId("1");
+        event.setCustomerId("고객1");
+        event.setRestaurantId("식당1");
+        event.setMenu("메뉴1");
+        event.setQuantity(2);
+        event.setPayment(new Money(20.0, "KRW"));
+
+        InventoryApplication.applicationContext = applicationContext;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String msg = objectMapper.writeValueAsString(event);
+
+            processor
+                .inboundTopic()
+                .send(
+                    MessageBuilder
+                        .withPayload(msg)
+                        .setHeader(
+                            MessageHeaders.CONTENT_TYPE,
+                            MimeTypeUtils.APPLICATION_JSON
+                        )
+                        .setHeader("type", event.getEventType())
+                        .build()
+                );
+
+            //then:
+
+            Message<String> received = (Message<String>) messageCollector
+                .forChannel(processor.outboundTopic())
+                .poll();
+
+            assertNotNull("Resulted event must be published", received);
+
+            LOGGER.info("Response received: {}", received.getPayload());
+
+            assertEquals(outputEvent.getMenuId(), "1");
+            assertEquals(outputEvent.getName(), "메뉴1");
+            assertEquals(outputEvent.getPrice(), new Money(10.0, "KRW"));
+            assertEquals(outputEvent.getDescription(), "메뉴1의 설명");
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            assertTrue("exception", false);
+        }
+    }
+}
